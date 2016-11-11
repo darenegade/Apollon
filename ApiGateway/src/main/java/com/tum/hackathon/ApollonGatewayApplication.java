c0447865b5ef60ca30ecd84d4a23e46e0a7968bd
René Zarwel <darenegade@gmx.de>
2016-11-11 22:26:48 +0100

    First Config Api Gateway

--- ApiGateway/src/main/java/com/tum/hackathon/ApollonGatewayApplication.java
+++ ApiGateway/src/main/java/com/tum/hackathon/ApollonGatewayApplication.java
@@ -1,9 +1,17 @@
 package com.tum.hackathon;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@Controller
+@EnableZuulProxy
 public class ApollonGatewayApplication {
 
 	public static void main(String[] args) {
