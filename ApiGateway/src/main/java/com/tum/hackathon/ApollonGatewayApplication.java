8da5e3cdefa0efc71f9183e8af4266ccec28e241
René Zarwel <darenegade@gmx.de>
2016-11-11 22:22:40 +0100

    Init Backend&APIGateway

---
+++ ApiGateway/src/main/java/com/tum/hackathon/ApollonGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.tum.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApollonGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApollonGatewayApplication.class, args);
+	}
+}
---
+++ Backend/src/test/java/com/tu/hackathon/ApollonBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tu.hackathon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApollonBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
