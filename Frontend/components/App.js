ee621f6b64db491a741214f5a694f70f93d47fe1
Bergi <a.d.bergi@web.de>
2016-11-12 20:49:56 +0100

    cleanup unused files and methods

--- Frontend/components/App.js
+++ Frontend/components/App.js
@@ -1,156 +1,42 @@
 var React = require('react');
 
 var Search = require('./Search');
-var Map = require('./Map');
-var CurrentLocation = require('./CurrentLocation');
-var LocationList = require('./LocationList');
-var MenuList = require('./MenuList');
+var NavigationMenu = require('./NavigationMenu');
 var HeaderIcon = require('./HeaderIcon');
-
-const headerItemStyle = {
-    height: '100%',
-    marginTop: '5px',
-    fontSize: '40px'
-};
-
 
 var App = React.createClass({
 
 	getInitialState(){
-
 		return {
             currentview: "index"
         }
 	},
 
-	toggleFavorite(address){
-
-		if(this.isAddressInFavorites(address)){
-			this.removeFromFavorites(address);
-		}
-		else{
-			this.addToFavorites(address);
-		}
-
-	},
-
-	addToFavorites(address){
-
-		var favorites = this.state.favorites;
-
-		favorites.push({
-			address: address,
-			timestamp: Date.now()
-		});
-
-		this.setState({
-			favorites: favorites
-		});
-
-		localStorage.favorites = JSON.stringify(favorites);
-	},
-
-	removeFromFavorites(address){
-
-		var favorites = this.state.favorites;
-		var index = -1;
-
-		for(var i = 0; i < favorites.length; i++){
-
-			if(favorites[i].address == address){
-				index = i;
-				break;
-			}
-
-		}
-
-		// If it was found, remove it from the favorites array
-
-		if(index !== -1){
-			
-			favorites.splice(index, 1);
-
-			this.setState({
-				favorites: favorites
-			});
-
-			localStorage.favorites = JSON.stringify(favorites);
-		}
-
-	},
-
-	isAddressInFavorites(address){
-
-		var favorites = this.state.favorites;
-
-		for(var i = 0; i < favorites.length; i++){
-
-			if(favorites[i].address == address){
-				return true;
-			}
-
-		}
-
-		return false;
-	},
-
-	searchForAddress(address){
-		
-		var self = this;
-
-		// We will use GMaps' geocode functionality,
-		// which is built on top of the Google Maps API
-
-		GMaps.geocode({
-			address: address,
-			callback: function(results, status) {
-
-				if (status !== 'OK') return;
-
-				var latlng = results[0].geometry.location;
-
-				self.setState({
-					currentAddress: results[0].formatted_address,
-					mapCoordinates: {
-						lat: latlng.lat(),
-						lng: latlng.lng()
-					}
-				});
-
-			}
-		});
-
-	},
-
 	render(){
-
+		const headerItemStyle = {
+			height: '100%',
+			marginTop: '5px',
+			fontSize: '40px'
+		};
 		return (
 		    <div>
-
-                <MenuList/>
-
+                <nav id="menu">
+					<NavigationMenu />
+				</nav>
                 <main id="panel">
-
                     <header>
                         <div className="col-xs-12 col-md-12 header-container">
                             <HeaderIcon/>
                             <div className="col-xs-8 col-md-8"><h1 className="heading-apollon" style={headerItemStyle}>Apollon</h1></div>
                         </div>
                     </header>
-
                     {
                         <Search onSearch={this.searchForAddress} />
                     }
-                    
-
                 </main>
             </div>
-
-
-
 		);
 	}
-
 });
 
 module.exports = App;
--- Frontend/components/NavigationMenu.js
+++ Frontend/components/NavigationMenu.js
@@ -2,14 +2,34 @@
  * Created by alexa on 12.11.2016.
  */
 var React = require('react');
-var MenuList = require('./MenuList');
 
 var NavigationMenu = React.createClass({
 
+    handleClick(element){
+        switch (element.id) {
+            case "myplaylist":
+        }
+        console.log("LALAL");
+    },
+
     render(){
         return (
-
+            <div className="list-group col-xs-12 col-md-6 col-md-offset-3">
+                <a className="list-group-item" onClick={this.handleClick} id="">
+                    Search Playlist
+                </a>
+                <a className="list-group-item" onClick={this.handleClick} id="playlist">
+                    Browse Playlist
+                </a>
+                <a className="list-group-item" onClick={this.handleClick} id="wishlist">
+                    Wishlist
+                </a>
+                <a className="list-group-item" onClick={this.handleClick} id="current">
+                    Current
+                </a>
+            </div>
         )
+
     }
 
 });
