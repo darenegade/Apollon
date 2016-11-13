da12b053a7c06aeef1828e19c20a6b47e771b3c5
Christian Hartinger <hartinge@hm.edu>
2016-11-13 05:20:21 +0100

    current song display

--- Frontend/components/CurrentSong.js
+++ Frontend/components/CurrentSong.js
@@ -7,27 +7,26 @@ var CurrentSong = React.createClass({
         boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
     },
     ImageStyle: {
-        height: '123px',
-        width: '110px',
+        height: '90px',
+        width: '90px',
     },
 
 
     render(){
         return <div className="panel panel-default" style={this.PanelDefaultStyle}>
             <div className="panel-body">
-                <div className="col-xs-4 col-md-4 no-padding">
+                <div className="col-xs-2 col-md-2 no-padding">
                     <img src={this.props.song.imageUrl} style={this.ImageStyle}/>
                 </div>
-                <div className="col-xs-8 col-md-8 no-padding">
+                <div className="col-xs-10 col-md-10 no-padding">
                     <div className="col-xs-8 col-md-8 no-padding">
-                        <div className="artist-name">{this.props.song.artistName}</div>
-                        <div className="song-name">{this.props.song.name}</div>
-                        <div className="smalltext">{this.props.song.albumName}</div>
+                        <div className="artist-current">{this.props.song.artistName}</div>
+                        <div className="song-current">{this.props.song.name}</div>
                     </div>
-                </div>
-                <div className="col-xs-4 col-md-4">
-                    <img src="/img/equalizer.gif" style={this.ImageStyle} ></img>
+                    <div className="col-xs-4 col-md-4 text-right">
 
+                        <img src="/img/equalizer.gif" style={this.ImageStyle} ></img>
+                    </div>
                 </div>
             </div>
         </div>
